aa99314c7ced9625825951318cb73bfb23af12ba
eliranroffe1990 <eliranr1990@gmail.com>
2025-12-04 07:49:04 +0200

    Add files via upload

---
+++ skip-app-v2/skip-app-v2/sw.js
@@ -0,0 +1,43 @@
+const VERSION = 'skip-v5';
+
+// Install - skip waiting immediately
+self.addEventListener('install', event => {
+  self.skipWaiting();
+});
+
+// Activate - clear ALL old caches and take control
+self.addEventListener('activate', event => {
+  event.waitUntil(
+    caches.keys().then(keys => {
+      return Promise.all(
+        keys.map(key => {
+          if (key !== VERSION) {
+            console.log('Deleting old cache:', key);
+            return caches.delete(key);
+          }
+        })
+      );
+    }).then(() => {
+      return self.clients.claim();
+    })
+  );
+});
+
+// Fetch - Network first, then cache
+self.addEventListener('fetch', event => {
+  event.respondWith(
+    fetch(event.request)
+      .then(response => {
+        // Clone and cache
+        const clone = response.clone();
+        caches.open(VERSION).then(cache => {
+          cache.put(event.request, clone);
+        });
+        return response;
+      })
+      .catch(() => {
+        // Offline - try cache
+        return caches.match(event.request);
+      })
+  );
+});
