c02f15ad08a1d820574bd061a1df2dff353f1504
eliranroffe1990 <eliranr1990@gmail.com>
2025-12-07 11:31:05 +0200

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,38 +1,53 @@
-// SKIPAPP Service Worker v2.0
-const CACHE_NAME = 'skipapp-v2.0';
-const urlsToCache = [
+
+// SKIPAPP Service Worker v3
+const CACHE_NAME = 'skipapp-v3';
+const CORE_ASSETS = [
   '/',
   '/index.html',
-  '/manifest.json'
+  '/manifest.json',
+  '/icons/icon-192.png',
+  '/icons/icon-512.png',
+  '/icons/icon-180.png'
 ];
 
-// Install
-self.addEventListener('install', event => {
+self.addEventListener('install', (event) => {
+  self.skipWaiting();
   event.waitUntil(
-    caches.open(CACHE_NAME)
-      .then(cache => cache.addAll(urlsToCache))
+    caches.open(CACHE_NAME).then((cache) => cache.addAll(CORE_ASSETS))
   );
 });
 
-// Fetch
-self.addEventListener('fetch', event => {
-  event.respondWith(
-    fetch(event.request)
-      .catch(() => caches.match(event.request))
+self.addEventListener('activate', (event) => {
+  event.waitUntil(
+    caches.keys().then((keys) => Promise.all(keys.map((k) => k !== CACHE_NAME && caches.delete(k))))
   );
+  self.clients.claim();
 });
 
-// Activate
-self.addEventListener('activate', event => {
-  event.waitUntil(
-    caches.keys().then(cacheNames => {
-      return Promise.all(
-        cacheNames.map(cacheName => {
-          if (cacheName !== CACHE_NAME) {
-            return caches.delete(cacheName);
-          }
+// Network-first for HTML, cache-first for others
+self.addEventListener('fetch', (event) => {
+  const req = event.request;
+  const isHTML = req.headers.get('accept')?.includes('text/html');
+
+  if (isHTML) {
+    event.respondWith(
+      fetch(req)
+        .then((res) => {
+          const copy = res.clone();
+          caches.open(CACHE_NAME).then((cache) => cache.put(req, copy));
+          return res;
         })
-      );
-    })
-  );
+        .catch(() => caches.match(req).then((res) => res || caches.match('/index.html')))
+    );
+  } else {
+    event.respondWith(
+      caches.match(req).then((cached) =>
+        cached || fetch(req).then((res) => {
+          const copy = res.clone();
+          caches.open(CACHE_NAME).then((cache) => cache.put(req, copy));
+          return res;
+        })
+      )
+    );
+  }
 });
