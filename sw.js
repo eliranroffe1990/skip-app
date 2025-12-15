bb6062b2d744a7f6b5a829dbcbfa4d8c0a3feaf6
eliranroffe1990 <eliranr1990@gmail.com>
2025-12-15 10:44:31 +0200

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,6 +1,6 @@
 
-// SKIPAPP Service Worker v4.8 (relative paths)
-const CACHE='skipapp-v4.8';
+// SKIPAPP Service Worker v4.9 (relative paths + cache bust)
+const CACHE='skipapp-v4.9';
 const CORE=['.','index.html','manifest.json','icon/icon-192.png','icon/icon-512.png','icon/icon-180.png','images/hero.jpg','images/play_logo.png'];
 self.addEventListener('install',e=>{self.skipWaiting(); e.waitUntil(caches.open(CACHE).then(c=>c.addAll(CORE)));});
 self.addEventListener('activate',e=>{e.waitUntil((async()=>{const keys=await caches.keys(); await Promise.all(keys.map(k=>k!==CACHE&&caches.delete(k)));})()); self.clients.claim();});
