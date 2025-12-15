f06eb555632499c5553ac85d180617a8102666b5
eliranroffe1990 <eliranr1990@gmail.com>
2025-12-15 11:59:59 +0200

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,6 +1,6 @@
 
-const CACHE='skipapp-v5.1';
-const CORE=['.','index.html','manifest.json','icon/icon-192.png','icon/icon-512.png','icon/icon-180.png','images/hero.jpg','images/play_logo.png'];
-self.addEventListener('install',e=>{self.skipWaiting(); e.waitUntil(caches.open(CACHE).then(c=>c.addAll(CORE)));});
-self.addEventListener('activate',e=>{e.waitUntil((async()=>{const keys=await caches.keys(); await Promise.all(keys.map(k=>k!==CACHE&&caches.delete(k)));})()); self.clients.claim();});
-self.addEventListener('fetch',e=>{const req=e.request; const html=req.headers.get('accept')?.includes('text/html'); if(html){e.respondWith(fetch(req).then(res=>{const copy=res.clone(); caches.open(CACHE).then(c=>c.put(req,copy)); return res;}).catch(()=>caches.match(req).then(r=>r||caches.match('index.html'))));} else {e.respondWith(caches.match(req).then(c=>c||fetch(req).then(res=>{const copy=res.clone(); caches.open(CACHE).then(cc=>cc.put(req,copy)); return res;})));}});
+var CACHE='skipapp-v5.1.1';
+var CORE=['.','index.html','manifest.json','icon/icon-192.png','icon/icon-512.png','icon/icon-180.png','images/hero.jpg','images/play_logo.png'];
+self.addEventListener('install', function(e){ self.skipWaiting(); e.waitUntil(caches.open(CACHE).then(function(c){ return c.addAll(CORE); })); });
+self.addEventListener('activate', function(e){ e.waitUntil(caches.keys().then(function(keys){ return Promise.all(keys.map(function(k){ if(k!==CACHE){ return caches.delete(k); } })); })); self.clients.claim(); });
+self.addEventListener('fetch', function(e){ var req=e.request; var accept=req.headers.get('accept'); var html = accept && accept.indexOf('text/html')!==-1; if(html){ e.respondWith(fetch(req).then(function(res){ var copy=res.clone(); caches.open(CACHE).then(function(c){ c.put(req, copy); }); return res; }).catch(function(){ return caches.match(req).then(function(r){ return r || caches.match('index.html'); }); })); } else { e.respondWith(caches.match(req).then(function(c){ return c || fetch(req).then(function(res){ var copy=res.clone(); caches.open(CACHE).then(function(cc){ cc.put(req, copy); }); return res; }); })); } });
